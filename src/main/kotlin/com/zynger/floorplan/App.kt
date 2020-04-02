fc8a6c50e648b2d921725c4496c088c4b466a357
Julio Zynger <julio.zynger@soundcloud.com>
2020-04-02 14:56:23 +0200

    Add gradle serialization dependency

--- src/main/kotlin/com/zynger/floorplan/App.kt
+++ src/main/kotlin/com/zynger/floorplan/App.kt
@@ -2,13 +2,16 @@ package com.zynger.floorplan
 
 import com.zynger.floorplan.model.Table
 import java.io.File
+import kotlinx.serialization.*
+import kotlinx.serialization.json.*
+
+@Serializable
+data class Data(val a: Int, val b: String = "42")
 
 fun main() {
     val src = File("samples/db.json")
     val table = Table("Users")
     println("Hello World $table")
 
-    src.forEachLine {
-        println(it)
-    }
+
 }
