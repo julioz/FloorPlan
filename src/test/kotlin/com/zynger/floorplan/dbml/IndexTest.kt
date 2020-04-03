463b5b62d643882fefffd0d252e5c09502b1419c
Julio Zynger <julio.zynger@soundcloud.com>
2020-04-03 15:03:01 +0200

    Test index rendering

---
+++ src/test/kotlin/com/zynger/floorplan/dbml/IndexTest.kt
@@ -0,0 +1,67 @@
+package com.zynger.floorplan.dbml
+
+import com.zynger.floorplan.model.Index as DbIndex
+import org.junit.Assert
+import org.junit.Test
+
+class IndexTest {
+
+    companion object {
+        private const val INDEX_NAME = "aRandomIndex"
+        private const val COLUMN_NAME = "username"
+        private const val CREATE_SQL = "SELECT * FROM MyTable"
+    }
+
+    @Test
+    fun `list single column index`() {
+        val index = DbIndex(
+            INDEX_NAME,
+            unique = false,
+            columnNames = listOf(COLUMN_NAME),
+            createSql = CREATE_SQL
+        )
+
+        Assert.assertEquals(
+            """
+                ($COLUMN_NAME) [name:'$INDEX_NAME']
+            """.trimIndent(),
+            Index(index).toString()
+        )
+    }
+
+    @Test
+    fun `lists multiple column index`() {
+        val column2 = "col2"
+        val column3 = "col3"
+        val index = DbIndex(
+            INDEX_NAME,
+            unique = false,
+            columnNames = listOf(COLUMN_NAME, column2, column3),
+            createSql = CREATE_SQL
+        )
+
+        Assert.assertEquals(
+            """
+                ($COLUMN_NAME,$column2,$column3) [name:'$INDEX_NAME']
+            """.trimIndent(),
+            Index(index).toString()
+        )
+    }
+
+    @Test
+    fun `adds uniqueness note when index is unique`() {
+        val index = DbIndex(
+            INDEX_NAME,
+            unique = true,
+            columnNames = listOf(COLUMN_NAME),
+            createSql = CREATE_SQL
+        )
+
+        Assert.assertEquals(
+            """
+                ($COLUMN_NAME) [name:'$INDEX_NAME', unique]
+            """.trimIndent(),
+            Index(index).toString()
+        )
+    }
+}
