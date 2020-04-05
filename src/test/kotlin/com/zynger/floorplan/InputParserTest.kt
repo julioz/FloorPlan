ed26ff9671e4cda53ba4cfd048b422254bc57c7e
Julio Zynger <julio.zynger@soundcloud.com>
2020-04-05 18:12:28 +0200

    Test cases

--- src/test/kotlin/com/zynger/floorplan/InputParserTest.kt
+++ src/test/kotlin/com/zynger/floorplan/InputParserTest.kt
@@ -1,8 +1,8 @@
 package com.zynger.floorplan
 
-import org.junit.Assert.assertEquals
-import org.junit.Assert.fail
+import org.junit.Assert.*
 import org.junit.Test
+import java.lang.IllegalArgumentException
 import java.lang.UnsupportedOperationException
 
 
@@ -12,7 +12,7 @@ class InputParserTest {
     fun `parses absolute file paths`() {
         val path = "samples/db.json"
 
-        val input = InputParser.parse(path)
+        val input = InputParser.parse(arrayOf(path))
 
         assertEquals(path, input.schemaPath)
     }
@@ -22,7 +22,7 @@ class InputParserTest {
         val path = "~/foo/bar/db.json"
         val expected = System.getProperty("user.home") + "/foo/bar/db.json"
 
-        val input = InputParser.parse(path)
+        val input = InputParser.parse(arrayOf(path))
 
         assertEquals(expected, input.schemaPath)
     }
@@ -32,10 +32,67 @@ class InputParserTest {
         val path = "~otheruser/Documents/foo/bar/db.json"
 
         try {
-            InputParser.parse(path)
+            InputParser.parse(arrayOf(path))
             fail()
         } catch(e: UnsupportedOperationException) {
             // ok
         }
+    }
+
+    @Test
+    fun `does not pick up output path argument if not specified`() {
+        val path = "samples/db.json"
+
+        val input = InputParser.parse(arrayOf(path))
+
+        assertNull(input.outputPath)
+    }
+
+    @Test
+    fun `throws when output argument is specified with no value`() {
+        val args = "samples/db.json --output="
+            .split(" ")
+            .toTypedArray()
+
+        try {
+            InputParser.parse(args)
+            fail()
+        } catch (e: IllegalArgumentException) {
+            // ok
+        }
+    }
+
+    @Test
+    fun `parses absolute file output paths`() {
+        val args = "samples/db.json --output=result/mydb.dbml"
+            .split(" ")
+            .toTypedArray()
+
+        val input = InputParser.parse(args)
+
+        assertEquals("result/mydb.dbml", input.outputPath)
+    }
+
+    @Test
+    fun `parses absolute file output paths by ignoring following arguments`() {
+        val args = "samples/db.json --output=result/mydb.dbml --something-else"
+            .split(" ")
+            .toTypedArray()
+
+        val input = InputParser.parse(args)
+
+        assertEquals("result/mydb.dbml", input.outputPath)
+    }
+
+    @Test
+    fun `parses relative file output paths`() {
+        val args = "samples/db.json --output=~/foo/bar/result/mydb.dbml"
+            .split(" ")
+            .toTypedArray()
+        val expected = System.getProperty("user.home") + "/foo/bar/result/mydb.dbml"
+
+        val input = InputParser.parse(args)
+
+        assertEquals(expected, input.outputPath)
     }
 }
