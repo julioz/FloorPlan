7e709c1964b2526dcdbce6e330175adf7595fe12
Julio Zynger <julio.zynger@soundcloud.com>
2020-05-18 19:53:44 +0200

    Cover ColumnReferenceParser with tests

--- dbml-parser/src/main/kotlin/com/zynger/floorplan/lex/ColumnReferenceParser.kt
+++ dbml-parser/src/main/kotlin/com/zynger/floorplan/lex/ColumnReferenceParser.kt
@@ -4,19 +4,23 @@ import com.zynger.floorplan.Reference
 import com.zynger.floorplan.ReferenceOrder
 
 object ColumnReferenceParser {
-    private val COLUMN_REFERENCE_REGEX = Regex("""((ref)\s*:\s*([<>-])\s*("\w+"|\w+)\.("\w+"|\w+))""")
+    private val COLUMN_REFERENCE_REGEX = Regex("""(([Rr]ef)\s*:\s*([<>-])\s*("\w+"|\w+)\.("\w+"|\w+))""")
 
     fun parse(tableName: String, fromColumn: String, columnProperties: String): Reference? {
         return if (COLUMN_REFERENCE_REGEX.containsMatchIn(columnProperties)) {
             val referenceProperties = COLUMN_REFERENCE_REGEX.find(columnProperties)!!
             Reference(
                 rawValue = referenceProperties.groups[0]!!.value,
-                fromTable = tableName,
-                fromColumn = fromColumn,
+                fromTable = tableName.removeSurroundQuotes(),
+                fromColumn = fromColumn.removeSurroundQuotes(),
                 referenceOrder = ReferenceOrder.fromString(referenceProperties.groups[3]!!.value),
-                toTable = referenceProperties.groups[4]!!.value,
-                toColumn = referenceProperties.groups[5]!!.value
+                toTable = referenceProperties.groups[4]!!.value.removeSurroundQuotes(),
+                toColumn = referenceProperties.groups[5]!!.value.removeSurroundQuotes()
             )
         } else null
+    }
+
+    private fun String.removeSurroundQuotes(): String {
+        return this.removeSurrounding("\"")
     }
 }
