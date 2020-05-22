355a2797e453300bdfcae65da98793b203e5d296
Julio Zynger <julio.zynger@soundcloud.com>
2020-05-22 18:40:09 +0200

    WIP: Create package to house String renderer

---
+++ core/src/main/kotlin/com/zynger/floorplan/dbml/render/DbmlRenderer.kt
@@ -0,0 +1,9 @@
+package com.zynger.floorplan.dbml.render
+
+import com.zynger.floorplan.dbml.Project
+
+object DbmlRenderer {
+    fun render(project: Project): String {
+        return "je"
+    }
+}
--- floorplan-cli/src/main/kotlin/com/zynger/floorplan/App.kt
+++ floorplan-cli/src/main/kotlin/com/zynger/floorplan/App.kt
@@ -1,26 +1,32 @@
 package com.zynger.floorplan
 
+import com.zynger.floorplan.dbml.Project
 import com.zynger.floorplan.dbml.Table
+import com.zynger.floorplan.dbml.render.DbmlRenderer
 import com.zynger.floorplan.room.Database
 import com.zynger.floorplan.room.RoomConsumer
 import java.io.File
 
 fun main(args: Array<String>) {
-    val input = InputParser.parse(args)
-    val src = File(input.schemaPath)
-    val database: Database = RoomConsumer.read(src)
+    val src = File("samples/db-track-pol.json")
+    val project: Project = RoomConsumer.read(src)
+    println(DbmlRenderer.render(project))
 
-    val settings = Settings(input.creationSqlAsTableNote, input.renderNullableFields)
-    val dbml = database
-        .entities
-        .map { Table(it, settings) }
-        .joinToString(separator = "\n\n")
-
-    if (input.outputPath == null) {
-        print(dbml)
-    } else {
-        val outputFile = File(input.outputPath)
-        outputFile.parentFile.mkdirs()
-        outputFile.writeText(dbml)
-    }
+//    val input = InputParser.parse(args)
+//    val src = File(input.schemaPath)
+//    val database: Database = RoomConsumer.read(src)
+//
+//    val settings = Settings(input.creationSqlAsTableNote, input.renderNullableFields)
+//    val dbml = database
+//        .entities
+//        .map { Table(it, settings) }
+//        .joinToString(separator = "\n\n")
+//
+//    if (input.outputPath == null) {
+//        print(dbml)
+//    } else {
+//        val outputFile = File(input.outputPath)
+//        outputFile.parentFile.mkdirs()
+//        outputFile.writeText(dbml)
+//    }
 }
