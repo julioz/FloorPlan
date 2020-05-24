7695d88c80fa56c4b15b00d5fcc7c1db5424f522
Julio Zynger <julio.zynger@soundcloud.com>
2020-05-24 12:33:07 +0200

    Render indexes

--- core/src/main/kotlin/com/zynger/floorplan/dbml/render/IndexRenderer.kt
+++ core/src/main/kotlin/com/zynger/floorplan/dbml/render/IndexRenderer.kt
@@ -1,9 +1,26 @@
 package com.zynger.floorplan.dbml.render
 
 import com.zynger.floorplan.dbml.Index
+import java.lang.StringBuilder
 
-class IndexRenderer(private val index: Index) {
+class IndexRenderer(
+    private val index: Index
+) {
     fun render(): String {
-        return "Index ${index.name}"
+        return StringBuilder()
+            .append("(")
+            .append(index.columnNames.joinToString(","))
+            .append(")")
+            .append(" ")
+            .append("[")
+            .append("name:")
+            .append("'${index.name}'")
+            .apply {
+                if (index.unique) {
+                    append(", unique")
+                }
+            }
+            .append("]")
+            .toString()
     }
 }
