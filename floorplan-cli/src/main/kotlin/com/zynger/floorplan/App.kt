52d92edd3455465f36808c90cdde7205b9601fe6
Julio Zynger <julio.zynger@soundcloud.com>
2020-05-24 12:41:52 +0200

    Update main function

--- floorplan-cli/src/main/kotlin/com/zynger/floorplan/App.kt
+++ floorplan-cli/src/main/kotlin/com/zynger/floorplan/App.kt
@@ -6,26 +6,31 @@ import com.zynger.floorplan.room.RoomConsumer
 import java.io.File
 
 fun main(args: Array<String>) {
+    hardcoded()
+
+//    cmdline(args)
+}
+
+private fun hardcoded() {
     val src = File("samples/db-track-pol.json")
     val project: Project = RoomConsumer.read(src)
     val settings = Settings(false, false) // TODO
     println(DbmlRenderer.render(project, settings))
+}
 
-//    val input = InputParser.parse(args)
-//    val src = File(input.schemaPath)
-//    val database: Database = RoomConsumer.read(src)
-//
-//    val settings = Settings(input.creationSqlAsTableNote, input.renderNullableFields)
-//    val dbml = database
-//        .entities
-//        .map { Table(it, settings) }
-//        .joinToString(separator = "\n\n")
-//
-//    if (input.outputPath == null) {
-//        print(dbml)
-//    } else {
-//        val outputFile = File(input.outputPath)
-//        outputFile.parentFile.mkdirs()
-//        outputFile.writeText(dbml)
-//    }
+private fun cmdline(args: Array<String>) {
+    val input = InputParser.parse(args)
+    val src = File(input.schemaPath)
+    val project: Project = RoomConsumer.read(src)
+
+    val settings = Settings(input.creationSqlAsTableNote, input.renderNullableFields)
+    val dbml = DbmlRenderer.render(project, settings)
+
+    if (input.outputPath == null) {
+        print(dbml)
+    } else {
+        val outputFile = File(input.outputPath)
+        outputFile.parentFile.mkdirs()
+        outputFile.writeText(dbml)
+    }
 }
