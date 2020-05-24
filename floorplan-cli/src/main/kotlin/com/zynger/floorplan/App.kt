3dd33982073e559993c70c80dc70edd17914d632
Julio Zynger <julio.zynger@soundcloud.com>
2020-05-24 13:18:34 +0200

    Remove hardcoded reference to app main

--- floorplan-cli/src/main/kotlin/com/zynger/floorplan/App.kt
+++ floorplan-cli/src/main/kotlin/com/zynger/floorplan/App.kt
@@ -6,24 +6,12 @@ import com.zynger.floorplan.room.RoomConsumer
 import java.io.File
 
 fun main(args: Array<String>) {
-    hardcoded()
-
-//    cmdline(args)
-}
-
-private fun hardcoded() {
-    val src = File("samples/db-track-pol.json")
-    val project: Project = RoomConsumer.read(src)
-    val settings = Settings(false, false) // TODO
-    println(DbmlRenderer.render(project, settings))
-}
-
-private fun cmdline(args: Array<String>) {
     val input = InputParser.parse(args)
+
     val src = File(input.schemaPath)
     val project: Project = RoomConsumer.read(src)
-
     val settings = Settings(input.creationSqlAsTableNote, input.renderNullableFields)
+
     val dbml = DbmlRenderer.render(project, settings)
 
     if (input.outputPath == null) {
