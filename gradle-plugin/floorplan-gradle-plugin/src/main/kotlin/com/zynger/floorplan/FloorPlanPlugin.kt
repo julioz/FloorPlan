03e40b8981bda3043a5429526dbc41bc07866a84
Segun Famisa <segun.famisa@soundcloud.com>
2020-05-28 18:24:44 +0200

    WIP: Getting the plugin to be accessible from other modules
    We used composite builds to make this happen
    
    Co-authored-by: Julio Zynger <julioz92@gmail.com>

---
+++ gradle-plugin/floorplan-gradle-plugin/src/main/kotlin/com/zynger/floorplan/FloorPlanPlugin.kt
@@ -0,0 +1,11 @@
+package com.zynger.floorplan
+
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+
+class FloorPlanPlugin: Plugin<Project> {
+
+    override fun apply(project: Project) {
+        println("JulioZ, hello!")
+    }
+}
