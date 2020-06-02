8f13f3f7f085fe680866672d7133f7bf67c7c5e8
Julio Zynger <julio.zynger@soundcloud.com>
2020-06-02 22:46:58 +0200

    Parse output format definition from gradle extension in plugin

--- gradle-plugin/floorplan-gradle-plugin/src/main/kotlin/com/zynger/floorplan/FloorPlanPlugin.kt
+++ gradle-plugin/floorplan-gradle-plugin/src/main/kotlin/com/zynger/floorplan/FloorPlanPlugin.kt
@@ -4,6 +4,7 @@ import org.gradle.api.Plugin
 import org.gradle.api.Project
 import org.gradle.api.plugins.ExtensionAware
 import java.io.File
+import java.lang.IllegalArgumentException
 
 class FloorPlanPlugin: Plugin<Project> {
 
@@ -24,7 +25,35 @@ class FloorPlanPlugin: Plugin<Project> {
     }
 
     private fun OutputFormatExtension.getFormat(): OutputFormat {
-        println(this)
-        return OutputFormat.SVG
+        ensureOnlyOneOutputFormatIsEnabled(this)
+        return when (val output = getEnabledOutputFormats(this).first()) {
+            is DbmlConfigurationExtension -> OutputFormat.DBML(
+                DbmlConfiguration(output.creationSqlAsTableNote.get(), output.renderNullableFields.get())
+            )
+            is SvgConfigurationExtension -> OutputFormat.SVG
+            is PngConfigurationExtension -> OutputFormat.PNG
+            is DotConfigurationExtension -> OutputFormat.DOT
+        }
+    }
+
+    private fun ensureOnlyOneOutputFormatIsEnabled(outputFormatExtension: OutputFormatExtension) {
+        val enabled = getEnabledOutputFormats(outputFormatExtension)
+
+        if (enabled.isEmpty()) {
+            throw IllegalArgumentException("There are no enabled output formats.")
+        }
+        if (enabled.size != 1) {
+            throw IllegalArgumentException("There can only be one enabled output format.")
+        }
+    }
+
+    private fun getEnabledOutputFormats(outputFormatExtension: OutputFormatExtension): List<OutputFormatConfiguration> {
+        val outputFormats = listOf(
+            outputFormatExtension.dbmlConfiguration,
+            outputFormatExtension.svgConfiguration,
+            outputFormatExtension.pngConfiguration,
+            outputFormatExtension.dotConfiguration
+        )
+        return outputFormats.associateWith { it.enabled.get() }.filter { it.value }.map { it.key }
     }
 }
