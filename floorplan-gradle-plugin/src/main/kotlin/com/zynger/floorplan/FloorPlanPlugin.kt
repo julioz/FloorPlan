7366ef81b7d0a9125235c7e6283a21b292b06908
Julio Zynger <julio.zynger@soundcloud.com>
2020-06-04 10:22:26 +0200

    Update dependencies and stub print call

--- floorplan-gradle-plugin/src/main/kotlin/com/zynger/floorplan/FloorPlanPlugin.kt
+++ floorplan-gradle-plugin/src/main/kotlin/com/zynger/floorplan/FloorPlanPlugin.kt
@@ -1,5 +1,7 @@
 package com.zynger.floorplan
 
+import com.zynger.floorplan.dbml.Reference
+import com.zynger.floorplan.dbml.ReferenceOrder
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 
@@ -8,10 +10,9 @@ class FloorPlanPlugin : Plugin<Project> {
     override fun apply(project: Project) {
         project.tasks.create("codeLines") {
             it.doLast {
-                println("Hello from CodeLinesCounterPlugin")
+                val ref = Reference("fromT", "fromC", "toT", "toC", ReferenceOrder.ManyToOne)
+                println("Hello from plugin! Ref is $ref")
             }
-        }.apply {
-            group = "stat"
         }
     }
 }
