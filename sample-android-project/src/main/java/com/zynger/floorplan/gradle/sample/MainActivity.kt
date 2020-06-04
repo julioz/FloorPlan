4f2dac8616ae2ecb7118ce70a599e19c7809b337
Julio Zynger <julio.zynger@soundcloud.com>
2020-06-04 10:48:30 +0200

    Setup sample android project to serve as testing target for gradle plugin

---
+++ sample-android-project/src/main/java/com/zynger/floorplan/gradle/sample/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.zynger.floorplan.gradle.sample
+
+import android.app.Activity
+import android.os.Bundle
+
+class MainActivity : Activity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
