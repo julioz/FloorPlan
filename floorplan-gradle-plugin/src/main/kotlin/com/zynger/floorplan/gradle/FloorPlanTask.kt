af4e46fe8f78d0c66fdb7f9010c9457664ec3f8d
Júlio Zynger <julioz92@gmail.com>
2020-06-07 15:55:17 +0200

    Gradle Plugin: Preserve directory structure (#37)
    Fixes https://github.com/julioz/FloorPlan/issues/33
    
    Room's directory structure when generating DB schemas adds the database definition package name and database name to a parent directory to prevent name clashes and conflicts.
    
    Here we move the schemas from our sample android application to respect Room's directory structure, and fix the issue that output representations are generated only on the root directory (defined by `outputLocation`) gradle property.
    
    So, we now preserve the directory structure and mimic it when generating output files.
    
    In other words, for a schema located in
    ```
    $schemaLocation/com.mypackage.name.MyDatabase
    ```
    the output files will be generated in
    ```
    $outputLocation/com.mypackage.name.MyDatabase
    ```
    
    This PR also pulls out the logic into a collaborator class to allow for unit testing of the gradle `Task` logic.

--- floorplan-gradle-plugin/src/main/kotlin/com/zynger/floorplan/gradle/FloorPlanTask.kt
+++ floorplan-gradle-plugin/src/main/kotlin/com/zynger/floorplan/gradle/FloorPlanTask.kt
@@ -1,11 +1,7 @@
 package com.zynger.floorplan.gradle
 
-import com.zynger.floorplan.Destination
-import com.zynger.floorplan.FloorPlan
-import com.zynger.floorplan.Format
-import com.zynger.floorplan.Output
+import com.zynger.floorplan.*
 import com.zynger.floorplan.dbml.Project
-import com.zynger.floorplan.DbmlConfiguration
 import com.zynger.floorplan.gradle.model.OutputFormat
 import com.zynger.floorplan.room.RoomConsumer
 import org.gradle.api.DefaultTask
@@ -28,9 +24,9 @@ open class FloorPlanTask : DefaultTask() {
         val schemas: List<File> = schemaLocation.findRoomSchemas()
         schemas.forEach { schema ->
             val project: Project = RoomConsumer.read(schema)
-            val outputFormat = outputFormat.mapOutputFormat()
-            val outputFileName = "${schema.nameWithoutExtension}.${outputFormat.extension}"
-            val outputFile = File(outputLocation, outputFileName)
+            val outputHandler = OutputParameterHandler(schema, schemaLocation)
+            val outputFormat: Format = outputHandler.format(outputFormat)
+            val outputFile: File = outputHandler.file(outputLocation, outputFormat)
 
             FloorPlan.render(
                 project = project,
@@ -41,19 +37,5 @@ open class FloorPlanTask : DefaultTask() {
 
     private fun File.findRoomSchemas(): List<File> {
         return walk().filter { it.extension == "json" }.toList()
-    }
-
-    private fun OutputFormat.mapOutputFormat(): Format {
-        return when (this) {
-            is OutputFormat.DBML -> Format.DBML(
-                DbmlConfiguration(
-                    this.config.creationSqlAsTableNote,
-                    this.config.renderNullableFields
-                )
-            )
-            OutputFormat.DOT -> Format.DOT
-            OutputFormat.SVG -> Format.SVG
-            OutputFormat.PNG -> Format.PNG
-        }
     }
 }
---
+++ floorplan-gradle-plugin/src/main/kotlin/com/zynger/floorplan/gradle/OutputParameterHandler.kt
@@ -0,0 +1,48 @@
+package com.zynger.floorplan.gradle
+
+import com.zynger.floorplan.DbmlConfiguration
+import com.zynger.floorplan.Format
+import com.zynger.floorplan.gradle.model.OutputFormat
+import java.io.File
+
+class OutputParameterHandler(
+    private val schema: File,
+    private val schemaLocation: File
+) {
+
+    fun format(outputFormat: OutputFormat): Format {
+        return outputFormat.mapOutputFormat()
+    }
+
+    private fun OutputFormat.mapOutputFormat(): Format {
+        return when (this) {
+            is OutputFormat.DBML -> Format.DBML(
+                DbmlConfiguration(
+                    this.config.creationSqlAsTableNote,
+                    this.config.renderNullableFields
+                )
+            )
+            OutputFormat.DOT -> Format.DOT
+            OutputFormat.SVG -> Format.SVG
+            OutputFormat.PNG -> Format.PNG
+        }
+    }
+
+    fun file(outputLocation: File, outputFormat: Format): File {
+        val outputFileName = "${schema.nameWithoutExtension}.${outputFormat.extension}"
+        return File(getOutputFileParentDirectory(outputLocation), outputFileName)
+    }
+
+    private fun getOutputFileParentDirectory(outputLocation: File): File {
+        val relativizedPathToSchemaInput = schemaLocation.toURI().relativize(schema.toURI()).path
+        return if (relativizedPathToSchemaInput.contains(File.separator)) {
+            // schema is stored under a sub-directory,
+            // we must relativize the directory structure to point to the parent dir.
+            val relativizedPath = relativizedPathToSchemaInput.substringBeforeLast(File.separator)
+            File(outputLocation.path + File.separator + relativizedPath)
+        } else {
+            // schema is directly under [outputLocation], so the parent is [outputLocation] itself.
+            outputLocation
+        }
+    }
+}
