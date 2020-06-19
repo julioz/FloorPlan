752b68ceae6b2d3671cdca462b56511e1805f1f8
Julio Zynger <julio.zynger@soundcloud.com>
2020-06-19 14:51:32 +0200

    Find table names

--- sqlite-consumer/src/main/kotlin/com/zynger/floorplan/sqlite/SqliteConsumer.kt
+++ sqlite-consumer/src/main/kotlin/com/zynger/floorplan/sqlite/SqliteConsumer.kt
@@ -2,9 +2,37 @@ package com.zynger.floorplan.sqlite
 
 import com.zynger.floorplan.dbml.Project
 import java.io.File
+import java.sql.Connection
+import java.sql.DriverManager
+import java.sql.ResultSet
 
 object SqliteConsumer {
     fun read(src: File): Project {
-        TODO("Hey $src")
+        DriverManager.getConnection("jdbc:sqlite:${src.path}").use {
+            val tables = it.getTables()
+
+            TODO("Hey $tables")
+        }
     }
+
+    private fun Connection.getTables(): List<Table> {
+        val tablesResultSet: ResultSet = createStatement()
+            .executeQuery(
+                """
+            SELECT name, sql FROM sqlite_master
+            WHERE type='table'
+            ORDER BY name;
+            """.trimIndent())
+
+        val tables = mutableListOf<Table>()
+        while (tablesResultSet.next()) {
+            tables += Table(
+                name = tablesResultSet.getString("name"),
+                sql = tablesResultSet.getString("sql")
+            )
+        }
+        return tables
+    }
+
+    private data class Table(val name: String, val sql: String)
 }
