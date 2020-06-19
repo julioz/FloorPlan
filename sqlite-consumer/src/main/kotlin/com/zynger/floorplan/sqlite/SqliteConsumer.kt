e07f89f5ea4581bdbe2a14d80f7e6de4fd650777
Julio Zynger <julio.zynger@soundcloud.com>
2020-06-19 15:02:22 +0200

    Query column names and properties

--- sqlite-consumer/src/main/kotlin/com/zynger/floorplan/sqlite/SqliteConsumer.kt
+++ sqlite-consumer/src/main/kotlin/com/zynger/floorplan/sqlite/SqliteConsumer.kt
@@ -1,5 +1,6 @@
 package com.zynger.floorplan.sqlite
 
+import com.zynger.floorplan.dbml.Column
 import com.zynger.floorplan.dbml.Project
 import java.io.File
 import java.sql.Connection
@@ -11,11 +12,19 @@ object SqliteConsumer {
         DriverManager.getConnection("jdbc:sqlite:${src.path}").use {
             val tables = it.getTables()
 
+            tables.forEach { table ->
+                val tableName = table.name
+                val columns = it.getColumns(tableName)
+
+                TODO("Hey $columns")
+
+            }
+
             TODO("Hey $tables")
         }
     }
 
-    private fun Connection.getTables(): List<Table> {
+    private fun Connection.getTables(): List<SQLiteTable> {
         val tablesResultSet: ResultSet = createStatement()
             .executeQuery(
                 """
@@ -24,9 +33,9 @@ object SqliteConsumer {
             ORDER BY name;
             """.trimIndent())
 
-        val tables = mutableListOf<Table>()
+        val tables = mutableListOf<SQLiteTable>()
         while (tablesResultSet.next()) {
-            tables += Table(
+            tables += SQLiteTable(
                 name = tablesResultSet.getString("name"),
                 sql = tablesResultSet.getString("sql")
             )
@@ -34,5 +43,21 @@ object SqliteConsumer {
         return tables
     }
 
-    private data class Table(val name: String, val sql: String)
+    private fun Connection.getColumns(tableName: String): List<Column> {
+        val columns = mutableListOf<Column>()
+        val columnsResultSet: ResultSet = createStatement().executeQuery("PRAGMA table_info($tableName);")
+        while (columnsResultSet.next()) {
+            columns += Column(
+                name = columnsResultSet.getString("name"),
+                type = columnsResultSet.getString("type"),
+                defaultValue = columnsResultSet.getString("Dflt_value"),
+                primaryKey = columnsResultSet.getBoolean("Pk"),
+                notNull = columnsResultSet.getBoolean("Notnull"),
+                increment = false // FIXME check indexes (https://stopbyte.com/t/how-to-check-if-a-column-is-autoincrement-primary-key-or-not-in-sqlite/174/2 or https://stackoverflow.com/questions/20979239/how-to-tell-if-a-sqlite-column-is-autoincrement)
+            )
+        }
+        return columns
+    }
+
+    private data class SQLiteTable(val name: String, val sql: String)
 }
