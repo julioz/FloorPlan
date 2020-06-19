89cd3151795fd619be01d76f4e525c80f578d7ed
Julio Zynger <julio.zynger@soundcloud.com>
2020-06-19 14:38:32 +0200

    Add sqlite-consumer module

---
+++ sqlite-consumer/src/main/kotlin/com/zynger/floorplan/sqlite/SqliteConsumer.kt
@@ -0,0 +1,10 @@
+package com.zynger.floorplan.sqlite
+
+import com.zynger.floorplan.dbml.Project
+import java.io.File
+
+object SqliteConsumer {
+    fun read(src: File): Project {
+        TODO("Hey $src")
+    }
+}
