e307f80ec3be2f26f24d97eb78e1616d6339f79e
Julio Zynger <julio.zynger@soundcloud.com>
2020-06-29 17:38:19 +0200

    Add icon for gutter action

---
+++ floorplan-intellij-plugin/src/main/kotlin/com/juliozynger/floorplan/ideaplugin/FloorPlanIcons.kt
@@ -0,0 +1,9 @@
+package com.juliozynger.floorplan.ideaplugin
+
+import com.intellij.openapi.util.IconLoader
+import javax.swing.Icon
+
+
+object FloorPlanIcons {
+    val FILE: Icon = IconLoader.getIcon("/com/floorplan/icons/go-to-schema.png")
+}
