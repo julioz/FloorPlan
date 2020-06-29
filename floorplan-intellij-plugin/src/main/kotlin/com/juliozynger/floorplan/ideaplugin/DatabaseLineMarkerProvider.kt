8238a9d1488f72d87f4732ad12b4b3ad51dd6313
Julio Zynger <julio.zynger@soundcloud.com>
2020-06-29 16:10:30 +0200

    Add Kotlin support

---
+++ floorplan-intellij-plugin/src/main/kotlin/com/juliozynger/floorplan/ideaplugin/DatabaseLineMarkerProvider.kt
@@ -0,0 +1,12 @@
+package com.juliozynger.floorplan.ideaplugin
+
+import com.intellij.codeInsight.daemon.LineMarkerInfo
+import com.intellij.codeInsight.daemon.LineMarkerProvider
+import com.intellij.psi.PsiElement
+
+class DatabaseLineMarkerProvider: LineMarkerProvider {
+    override fun getLineMarkerInfo(element: PsiElement): LineMarkerInfo<*> {
+        TODO("Not yet implemented")
+    }
+
+}
