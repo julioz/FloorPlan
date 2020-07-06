8d1c6922bf9e75c4969a973948a606e16cd13e0b
Julio Zynger <julio.zynger@soundcloud.com>
2020-07-06 20:59:26 +0200

    Remove unused file

--- sample-android-project/src/main/java/com/zynger/floorplan/gradle/sample/MyDatabase.kt
+++
@@ -1,9 +0,0 @@
-package com.zynger.floorplan.gradle.sample
-
-import androidx.room.Database
-import androidx.room.RoomDatabase
-
-@Database(
-    version = 1
-)
-abstract class MyDatabase : RoomDatabase()
