7af43c4ccb28e515cf2d39e2eb699be1cd1e3684
Julio Zynger <julio.zynger@soundcloud.com>
2020-07-08 12:33:27 +0200

    Generalize test assertions

--- floorplan-intellij-plugin/src/test/kotlin/com/juliozynger/floorplan/ideaplugin/DatabaseLineMarkerProviderTest.kt
+++ floorplan-intellij-plugin/src/test/kotlin/com/juliozynger/floorplan/ideaplugin/DatabaseLineMarkerProviderTest.kt
@@ -1,9 +1,11 @@
 package com.juliozynger.floorplan.ideaplugin
 
+import com.intellij.codeInsight.daemon.LineMarkerInfo
 import com.intellij.codeInsight.daemon.RelatedItemLineMarkerInfo
 import com.intellij.codeInsight.daemon.impl.DaemonCodeAnalyzerImpl
 import com.intellij.openapi.editor.Editor
 import com.intellij.openapi.project.Project
+import com.intellij.psi.PsiFile
 import com.intellij.psi.xml.XmlFile
 import com.intellij.testFramework.fixtures.LightJavaCodeInsightFixtureTestCase
 import com.intellij.util.ReflectionUtil
@@ -61,15 +63,10 @@ class DatabaseLineMarkerProviderTest: LightJavaCodeInsightFixtureTestCase() {
 
         myFixture.doHighlighting()
 
-        val infoList = DaemonCodeAnalyzerImpl.getLineMarkers(editor.document, project)
-        assertEquals(1, infoList.size)
-        val lineMarker = infoList.first()
-        assertEquals("Navigate to database ER diagram", lineMarker.lineMarkerTooltip)
-        assertTrue(lineMarker is RelatedItemLineMarkerInfo)
-        val relatedItem = (lineMarker as RelatedItemLineMarkerInfo).createGotoRelatedItems().first()
-        assertTrue(relatedItem.element is XmlFile)
-        val navTargetName = (relatedItem.element as XmlFile).name
-        assertEquals("diagram.svg", navTargetName)
+        DaemonCodeAnalyzerImpl.getLineMarkers(editor.document, project)
+            .assertNumberOfGutterMarkers(1)
+            .assertTooltipOnGutterMarkers("Navigate to database ER diagram")
+            .assertNavigationTargets(listOf("diagram.svg"))
     }
 
     fun testDatabaseAnnotationPresentWithSecondAnnotationInFile() {
@@ -84,22 +81,41 @@ class DatabaseLineMarkerProviderTest: LightJavaCodeInsightFixtureTestCase() {
 
         myFixture.doHighlighting()
 
-        val infoList = DaemonCodeAnalyzerImpl.getLineMarkers(editor.document, project)
-        assertEquals(1, infoList.size)
-        val lineMarker = infoList.first()
-        assertEquals("Navigate to database ER diagram", lineMarker.lineMarkerTooltip)
-        assertTrue(lineMarker is RelatedItemLineMarkerInfo)
-        val relatedItem = (lineMarker as RelatedItemLineMarkerInfo).createGotoRelatedItems().first()
-        assertTrue(relatedItem.element is XmlFile)
-        val navTargetName = (relatedItem.element as XmlFile).name
-        assertEquals("diagram.svg", navTargetName)
+        DaemonCodeAnalyzerImpl.getLineMarkers(editor.document, project)
+            .assertNumberOfGutterMarkers(1)
+            .assertTooltipOnGutterMarkers("Navigate to database ER diagram")
+            .assertNavigationTargets(listOf("diagram.svg"))
+    }
+
+    private fun List<LineMarkerInfo<*>>.assertNumberOfGutterMarkers(expectedNumberOfMarkers: Int): List<LineMarkerInfo<*>> {
+        assertEquals(expectedNumberOfMarkers, size)
+        return this
+    }
+
+    private fun List<LineMarkerInfo<*>>.assertTooltipOnGutterMarkers(expectedTooltip: String): List<LineMarkerInfo<*>> {
+        forEach {
+            assertEquals(expectedTooltip, it.lineMarkerTooltip)
+        }
+        return this
+    }
+
+    private fun List<LineMarkerInfo<*>>.assertNavigationTargets(targetFileNames: List<String>): List<LineMarkerInfo<*>> {
+        assertEquals(targetFileNames.size, size)
+        forEachIndexed { index, lineMarker ->
+            assertTrue(lineMarker is RelatedItemLineMarkerInfo)
+            val relatedItem = (lineMarker as RelatedItemLineMarkerInfo).createGotoRelatedItems().toList()[index]
+
+            assertTrue(relatedItem.element is PsiFile)
+            val navTargetName = (relatedItem.element as PsiFile).name
+            assertEquals(targetFileNames[index], navTargetName)
+        }
+        return this
     }
 
     override fun tearDown() {
         checkJavaSwingTimersAreDisposed()
         super.tearDown()
     }
-
     private fun checkJavaSwingTimersAreDisposed() {
         // NOTE: added this otherwise plugin tests fail due to swing timers not being disposed which have nothing to do with the plugin test
         // see https://intellij-support.jetbrains.com/hc/en-us/community/posts/360006918780-Tests-Fail-due-to-Java-Swing-Timers-Not-Disposed
@@ -129,4 +145,5 @@ class DatabaseLineMarkerProviderTest: LightJavaCodeInsightFixtureTestCase() {
             e.printStackTrace()
         }
     }
+
 }
