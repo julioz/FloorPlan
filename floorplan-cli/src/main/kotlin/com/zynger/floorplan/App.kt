65a56f4b29dc09d248df6c55664435ba9fd560da
Júlio Zynger <julioz92@gmail.com>
2020-07-27 21:19:04 +0200

    Use Clikt for CLI component (#52)
    Fixes #51
    
    Not only we simplify the code, with Clikt we get new features:
    - Multiple option/argument names
    - CLI Help
    
    <img width="899" alt="Screenshot 2020-07-27 at 21 15 18" src="https://user-images.githubusercontent.com/1541701/88581992-4ad94180-d04e-11ea-9a36-925c28f078a1.png">
    
    -----------------
    
    * Add dependency on Clikt
    
    * Hello World Clikt
    
    * Take in schema path as argument
    
    * Add output file path
    
    * Add creationSqlAsTableNote as boolean flag
    
    * Add renderNullableFields as boolean flag
    
    * Uppercase help
    
    * Paths instead of Files
    
    * Add formats with validation
    
    * Full integration replacing InputParser usage
    
    * Only reject invalid formats
    
    * Add simpler aliases for option names
    
    * Remove InputParser
    
    * Update docs

--- floorplan-cli/src/main/kotlin/com/zynger/floorplan/App.kt
+++ floorplan-cli/src/main/kotlin/com/zynger/floorplan/App.kt
@@ -1,51 +1,89 @@
 package com.zynger.floorplan
 
+import com.github.ajalt.clikt.core.CliktCommand
+import com.github.ajalt.clikt.parameters.arguments.argument
+import com.github.ajalt.clikt.parameters.options.flag
+import com.github.ajalt.clikt.parameters.options.option
+import com.github.ajalt.clikt.parameters.options.split
+import com.github.ajalt.clikt.parameters.options.validate
+import com.github.ajalt.clikt.parameters.types.file
+import com.github.ajalt.clikt.parameters.types.path
 import com.zynger.floorplan.dbml.Project
-import java.io.File
 import java.lang.IllegalArgumentException
 
-fun main(args: Array<String>) {
-    val input = InputParser.parse(args)
-
-    val src = File(input.schemaPath)
-
-    val project: Project = FloorPlanConsumerSniffer
-        .sniff(src)
-        .read(src)
-
-    val outputFormats = input.mapOutputFormats()
-
-    outputFormats.forEach { outputFormat ->
-        FloorPlan.render(
-            project = project,
-            output = Output(
-                outputFormat,
-                if (input.outputPath == null) Destination.StandardOut else Destination.Disk(File(input.outputPath))
-            )
-        )
+class FloorPlanCli: CliktCommand(
+    name = "floorplan",
+    help = "Render SCHEMAPATH as DBML or ER diagram."
+) {
+    private val onlyDbmlNote = "[note: only for DBML outputs]"
+    private val validFormats = listOf("dbml", "svg", "png", "dot")
+    private val schemaFile by argument().file(
+        mustExist = true,
+        canBeFile = true,
+        canBeDir = false
+    )
+    private val outputPath by option(
+        names = *arrayOf("-o", "--output"),
+        help = "Output file for the rendering content"
+    ).path(
+        mustExist = false,
+        canBeFile = true,
+        canBeDir = false
+    )
+    private val formats by option(
+        names = *arrayOf("--format", "-f"),
+        help = "Specify an output format for the rendering content [one or more of DBML, SVG, PNG, DOT]"
+    ).split(",").validate {
+        require(it.all { format -> validFormats.contains(format) }) {
+            "Unrecognized rendering format: ${it.minus(validFormats)}. Valid ones are ${validFormats.joinToString()}."
+        }
     }
-}
+    private val creationSqlAsTableNote by option(
+        "--creation-sql-as-table-note", "--ctn",
+        help = "Adds the SQL used to create tables as notes $onlyDbmlNote"
+    ).flag(default = false)
+    private val renderNullableFields by option(
+        "--render-nullable-fields", "--rnf",
+        help = "Changes the rendering of the data type of nullable fields $onlyDbmlNote"
+    ).flag(default = false)
 
-private fun InputParser.Input.mapOutputFormats(): List<Format> {
-    return formats?.map {
-        when (it.trim().toLowerCase()) {
-            "dbml" -> Format.DBML(
+    override fun run() {
+        val project: Project = FloorPlanConsumerSniffer
+            .sniff(schemaFile)
+            .read(schemaFile)
+
+        val outputFormats = formats?.map {
+            when (it.trim().toLowerCase()) {
+                "dbml" -> Format.DBML(
+                    DbmlConfiguration(
+                        creationSqlAsTableNote,
+                        renderNullableFields
+                    )
+                )
+                "svg" -> Format.SVG
+                "png" -> Format.PNG
+                "dot" -> Format.DOT
+                else -> throw IllegalArgumentException("Unrecognized rendering format: $it. Must be one of dbml, svg, png, dot.")
+            }
+        } ?: listOf(
+            Format.DBML(
                 DbmlConfiguration(
                     creationSqlAsTableNote,
                     renderNullableFields
                 )
             )
-            "svg" -> Format.SVG
-            "png" -> Format.PNG
-            "dot" -> Format.DOT
-            else -> throw IllegalArgumentException("Unrecognized rendering format: $it. Must be one of dbml, svg, png, dot.")
-        }
-    } ?: listOf(
-        Format.DBML(
-            DbmlConfiguration(
-                creationSqlAsTableNote,
-                renderNullableFields
-            )
         )
-    )
+
+        outputFormats.forEach { outputFormat ->
+            FloorPlan.render(
+                project = project,
+                output = Output(
+                    outputFormat,
+                    if (outputPath == null) Destination.StandardOut else Destination.Disk(outputPath!!.toFile())
+                )
+            )
+        }
+    }
 }
+
+fun main(args: Array<String>) = FloorPlanCli().main(args)
