0e58efd9d056ce940319aa816e285e593f40cd96
Tomasz Mościcki <tomasz.moscicki@sonalake.com>
2021-08-19 16:10:49 +0200

    Fixed code formatting (#62)

--- consumer-sniffer/src/main/kotlin/com/zynger/floorplan/FloorPlanConsumerSniffer.kt
+++ consumer-sniffer/src/main/kotlin/com/zynger/floorplan/FloorPlanConsumerSniffer.kt
@@ -7,12 +7,18 @@ import java.io.File
 
 object FloorPlanConsumerSniffer {
 
-    private val extensionToConsumer = mapOf("json" to RoomConsumer, "db" to SqliteConsumer, "dbml" to DbmlConsumer)
+    private val extensionToConsumer = mapOf(
+        "json" to RoomConsumer,
+        "db" to SqliteConsumer,
+        "dbml" to DbmlConsumer
+    )
 
     fun sniff(inputSourceFile: File): Consumer =
-        if (isConsumable(inputSourceFile))
+        if (isConsumable(inputSourceFile)) {
             extensionToConsumer.getValue(inputSourceFile.extension)
-        else throw IllegalArgumentException("Unknown file extension: ${inputSourceFile.extension}")
+        } else {
+            throw IllegalArgumentException("Unknown file extension: ${inputSourceFile.extension}")
+        }
 
     fun isConsumable(inputSourceFile: File): Boolean = extensionToConsumer.containsKey(inputSourceFile.extension)
 }
